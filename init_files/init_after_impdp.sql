54fb671cc54155a613decc8014d6f71d760fe46e
madjid kazi tani <jijid13@gmail.com>
2017-02-26 23:04:21 +0100

    sql correction

--- init_files/init_after_impdp.sql
+++ init_files/init_after_impdp.sql
@@ -1,3 +1,4 @@
+grant create synonym to user1;
 revoke create synonym from user1;
 
 exit;
--- init_files/init_before_impdp.sql
+++ init_files/init_before_impdp.sql
@@ -2,6 +2,7 @@ CREATE USER user1 IDENTIFIED BY oracle;
 
 GRANT CREATE SESSION TO user1;
 
+grant create synonym to user1;
 revoke create synonym from user1;
 
 exit;
